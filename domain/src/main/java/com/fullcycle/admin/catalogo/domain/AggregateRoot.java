2a2c9af35a1786100d1d7c65bc2495b64d1a5267
Rafael Paulo <rafael8paulo@gmail.com>
2024-02-10 16:33:10 -0300

    Implementando as classes do DDD

---
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/AggregateRoot.java
@@ -0,0 +1,9 @@
+package com.fullcycle.admin.catalogo.domain;
+
+public class AggregateRoot<ID extends Identifier> extends Entity<ID> {
+
+    protected AggregateRoot(ID id) {
+        super(id);
+    }
+    
+}
